9b79affd9617296f96f3d2e55b217e941277aaed
Walter Snyder <wsnyder007@gmail.com>
2018-12-01 17:36:19 -0500

    Whoops

---
+++ Tutor Thing/Tutor Thing/Models/Course.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Project_Tutor.Model {
+    public class Course {
+        public int Id;
+        public string Number;
+        public string Name;
+        public List<int> Attendees;
+
+        public Course(int id, string number, string name, List<int> attendees) {
+            Id = id;
+            Number = number;
+            Name = name;
+            Attendees = attendees;
+        }
+    }
+}
---
+++ Tutor Thing/Tutor Thing/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Project_Tutor.Model {
+    public class User {
+        public int Id;        
+        public string FirstName;
+        public string LastName;
+        public string Availability;
+        public string Bio;
+        public bool IsTutor;
+        public List<int> Courses;
+
+        public User(int id, string firstName, string lastName, string availability, string bio, bool isTutor, List<int> courses) {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Availability = availability;
+            Bio = bio;
+            IsTutor = isTutor;
+            Courses = courses;
+        }
+    }
+}
